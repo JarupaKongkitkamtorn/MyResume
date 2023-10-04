db8a3628465e3ca99af100ba62a965159127cd8a
่Jarupa Kongkitkamtorn <jarupa.kongkitkamtorn@gmail.com>
2023-10-04 12:00:41 +0700

    Update

--- src/container/Home/AboutMe.tsx
+++ src/container/Home/AboutMe.tsx
@@ -1,100 +1,134 @@
-import React, { useState, useEffect } from 'react';
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
-import Divider from '@mui/material/Divider';
-import Typography from '@mui/material/Typography';
-import Grow from '@mui/material/Grow';
+import React, { useState, useEffect } from "react";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import Divider from "@mui/material/Divider";
+import Typography from "@mui/material/Typography";
+import Grow from "@mui/material/Grow";
 
 //component
-import { InformationProps } from '../../App'
+import { InformationProps } from "../../App";
 interface AboutMeProps {
-    information: InformationProps,
-    matchesmd: boolean,
+  information: InformationProps;
+  matchesmd: boolean;
 }
 
 const AboutMe: React.FC<AboutMeProps> = ({ information, matchesmd }) => {
-    const [offset, setOffset] = useState<number>(0)
-    const [section, setSection] = useState<number>(0)
-    useEffect(() => {
-        document.addEventListener("scroll", trackScrolling, { passive: true })
-        const sections = document.getElementById("aboutme")
-        if (sections) {
-            setSection(sections.offsetTop / 2)
-        }
-        return () => {
-            window.removeEventListener("scroll", trackScrolling)
-        }
-    }, [])
-
-    const trackScrolling = () => {
-        const getOffset = document.body.scrollTop || document.documentElement.scrollTop
-        setOffset(getOffset)
-        const sections = document.getElementById("aboutme")
-        if (sections) {
-            setSection(sections.offsetTop / 2)
-        }
+  const [offset, setOffset] = useState<number>(0);
+  const [section, setSection] = useState<number>(0);
+  useEffect(() => {
+    document.addEventListener("scroll", trackScrolling, { passive: true });
+    const sections = document.getElementById("aboutme");
+    if (sections) {
+      setSection(sections.offsetTop / 2);
     }
-    return (
-        <div className="bgleft" id="aboutme">
-            <div className="container" >
-                <Grid container className='paddingtopbottom' justifyContent="center" alignItems="center">
-                    <Grid item lg={12} sm={12}>
-                        <Paper elevation={12} className='technical-skills-paper' >
-                            <Grid container spacing={matchesmd ? 3 : 4} justifyContent="center" alignItems="center">
-                                <Grid item lg={12} sm={12} className='header'>
-                                    <div>About Me</div>
-                                </Grid>
-                                <Grid item container lg={12} sm={12} spacing={6} sx={{ color: "#ffffff" }}>
-                                    <Grow
-                                        in={offset >= section}
-                                        style={{ transformOrigin: '0 0 0' }}
-                                        {...(offset >= section ? { timeout: 2000 } : {})}
-                                    >
-                                        <Grid item lg={6} sm={12}>
-                                            <Divider className='divider'>Education</Divider>
-                                            <Typography className='title'>{information.education.bachelor}</Typography>
-                                            <Typography className='title'>{information.education.university}</Typography>
-                                            <Typography className='description'>{information.education.gpa}</Typography>
-                                        </Grid>
-                                    </Grow>
-                                    <Grow
-                                        in={offset >= section}
-                                        style={{ transformOrigin: '0 0 0' }}
-                                        {...(offset >= section ? { timeout: 3000 } : {})}
-                                    >
-                                        <Grid item lg={6} sm={12}>
-                                            <Divider className='divider'>Training</Divider>
-                                            <ul>
-                                                {information.training.map((item: string, key: number) => <li key={key}>
-                                                    <Typography className='description'>{item}</Typography>
-                                                </li>
-                                                )}
-                                            </ul>
-                                        </Grid>
-                                    </Grow>
-                                    <Grow
-                                        in={offset >= section}
-                                        style={{ transformOrigin: '0 0 0' }}
-                                        {...(offset >= section ? { timeout: 4000 } : {})}
-                                    >
-                                        <Grid item lg={6} sm={12}>
-                                            <Divider className='divider'>Certification</Divider>
-                                            <ul>
-                                                {information.certification.map((item: string, key: number) => <li key={key}>
-                                                    <Typography className='description'>{item}</Typography>
-                                                </li>
-                                                )}
-                                            </ul>
-                                        </Grid>
-                                    </Grow>
-                                </Grid>
-                            </Grid>
-                        </Paper>
-                    </Grid>
+    return () => {
+      window.removeEventListener("scroll", trackScrolling);
+    };
+  }, []);
+
+  const trackScrolling = () => {
+    const getOffset =
+      document.body.scrollTop || document.documentElement.scrollTop;
+    setOffset(getOffset);
+    const sections = document.getElementById("aboutme");
+    if (sections) {
+      setSection(sections.offsetTop / 2);
+    }
+  };
+  return (
+    <div className="bgleft flex-center" id="aboutme">
+      <div className="container">
+        <Grid
+          container
+          className="paddingtopbottom"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Grid item lg={12} sm={12}>
+            <Paper elevation={12} className="technical-skills-paper">
+              <Grid
+                container
+                spacing={matchesmd ? 3 : 4}
+                justifyContent="center"
+                alignItems="center"
+              >
+                <Grid item lg={12} sm={12} className="header">
+                  <div>About Me</div>
                 </Grid>
-            </div>
-        </div>
-    );
-}
+                <Grid
+                  item
+                  container
+                  lg={12}
+                  sm={12}
+                  spacing={6}
+                  sx={{ color: "#ffffff" }}
+                >
+                  <Grow
+                    in={offset >= section}
+                    style={{ transformOrigin: "0 0 0" }}
+                    {...(offset >= section ? { timeout: 2000 } : {})}
+                  >
+                    <Grid item lg={6} sm={12}>
+                      <Divider className="divider">Education</Divider>
+                      <Typography className="title">
+                        {information.education.bachelor}
+                      </Typography>
+                      <Typography className="title">
+                        {information.education.university}
+                      </Typography>
+                      <Typography className="description">
+                        {information.education.gpa}
+                      </Typography>
+                    </Grid>
+                  </Grow>
+                  <Grow
+                    in={offset >= section}
+                    style={{ transformOrigin: "0 0 0" }}
+                    {...(offset >= section ? { timeout: 3000 } : {})}
+                  >
+                    <Grid item lg={6} sm={12}>
+                      <Divider className="divider">Training</Divider>
+                      <ul>
+                        {information.training.map(
+                          (item: string, key: number) => (
+                            <li key={key}>
+                              <Typography className="description">
+                                {item}
+                              </Typography>
+                            </li>
+                          )
+                        )}
+                      </ul>
+                    </Grid>
+                  </Grow>
+                  <Grow
+                    in={offset >= section}
+                    style={{ transformOrigin: "0 0 0" }}
+                    {...(offset >= section ? { timeout: 4000 } : {})}
+                  >
+                    <Grid item lg={6} sm={12}>
+                      <Divider className="divider">Certification</Divider>
+                      <ul>
+                        {information.certification.map(
+                          (item: string, key: number) => (
+                            <li key={key}>
+                              <Typography className="description">
+                                {item}
+                              </Typography>
+                            </li>
+                          )
+                        )}
+                      </ul>
+                    </Grid>
+                  </Grow>
+                </Grid>
+              </Grid>
+            </Paper>
+          </Grid>
+        </Grid>
+      </div>
+    </div>
+  );
+};
 
 export default AboutMe;
--- src/container/Home/Technicalskills.tsx
+++ src/container/Home/Technicalskills.tsx
@@ -1,75 +1,111 @@
-import React, { useState, useEffect } from 'react';
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
-import Grow from '@mui/material/Grow';
-import Typography from '@mui/material/Typography';
+import React, { useState, useEffect } from "react";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import Grow from "@mui/material/Grow";
+import Typography from "@mui/material/Typography";
 
 //component
-import { TechnicalskillsProps } from '../../App'
-
+import { TechnicalskillsProps } from "../../App";
 
 interface TechnicalSkillsProps {
-    information: TechnicalskillsProps[],
-    matchesmd: boolean,
+  information: TechnicalskillsProps[];
+  matchesmd: boolean;
 }
 
-const Technicalskills: React.FC<TechnicalSkillsProps> = ({ information, matchesmd }) => {
-    const [offset, setOffset] = useState<number>(0)
-    const [section, setSection] = useState<number>(0)
-    useEffect(() => {
-        document.addEventListener("scroll", trackScrolling, { passive: true })
-        const sections = document.getElementById("technical-skills")
-        if (sections) {
-            setSection(sections.offsetTop / 2)
-        }
-        return () => {
-            window.removeEventListener("scroll", trackScrolling)
-        }
-    }, [])
-
-    const trackScrolling = () => {
-        const getOffset = document.body.scrollTop || document.documentElement.scrollTop
-        setOffset(getOffset)
-        const sections = document.getElementById("technical-skills")
-        if (sections) {
-            setSection(sections.offsetTop / 2)
-        }
+const Technicalskills: React.FC<TechnicalSkillsProps> = ({
+  information,
+  matchesmd,
+}) => {
+  const [offset, setOffset] = useState<number>(0);
+  const [section, setSection] = useState<number>(0);
+  useEffect(() => {
+    document.addEventListener("scroll", trackScrolling, { passive: true });
+    const sections = document.getElementById("technical-skills");
+    if (sections) {
+      setSection(sections.offsetTop / 2);
     }
-    const renderItem = (item: TechnicalskillsProps, key: number) => {
-        return <Grid item lg={2} sm={3} key={key}>
-            <Grow
-                in={offset >= section}
-                style={{ transformOrigin: '0 0 0' }}
-                {...(offset >= section ? { timeout: !key ? 500 : key * 500 } : {})}
-            >
-                <Grid container className='item' justifyContent="center" alignItems="center">
-                    <Grid item container lg={12} sm={12} className='center'>
-                        <Grid item lg={12} sm={12} className='center'>
-                            <img src={item.icon} alt='iconSkills' width="40px" height="auto" />
-                            <Typography className='description' style={{ color: "#ffffff" }}>{item.label}</Typography>
-                        </Grid>
-                    </Grid>
-                </Grid >
-            </Grow>
-        </Grid>
+    return () => {
+      window.removeEventListener("scroll", trackScrolling);
+    };
+  }, []);
 
+  const trackScrolling = () => {
+    const getOffset =
+      document.body.scrollTop || document.documentElement.scrollTop;
+    setOffset(getOffset);
+    const sections = document.getElementById("technical-skills");
+    if (sections) {
+      setSection(sections.offsetTop / 2);
     }
+  };
+  const renderItem = (item: TechnicalskillsProps, key: number) => {
     return (
-        <div className="bgcom container" id="technical-skills">
-            <Grid container justifyContent="center" alignItems="center" className='paddingtopbottom'>
-                <Grid item lg={12} md={12} className='animate__animated animate__fadeInUp'>
-                    <Paper elevation={12} className='technical-skills-paper' >
-                        <Grid container spacing={matchesmd ? 3 : 4} justifyContent="center" alignItems="center">
-                            <Grid item lg={12} sm={12} className='header'>
-                                <div>Technical Skills</div>
-                            </Grid>
-                            {information.map((item: TechnicalskillsProps, key: number) => renderItem(item, key))}
-                        </Grid>
-                    </Paper>
-                </Grid>
+      <Grid item lg={2} sm={3} key={key}>
+        <Grow
+          in={offset >= section}
+          style={{ transformOrigin: "0 0 0" }}
+          {...(offset >= section ? { timeout: !key ? 500 : key * 500 } : {})}
+        >
+          <Grid
+            container
+            className="item"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Grid item container lg={12} sm={12} className="center">
+              <Grid item lg={12} sm={12} className="center">
+                <img
+                  src={item.icon}
+                  alt="iconSkills"
+                  width="40px"
+                  height="auto"
+                />
+                <Typography
+                  className="description"
+                  style={{ color: "#ffffff" }}
+                >
+                  {item.label}
+                </Typography>
+              </Grid>
             </Grid>
-        </div>
+          </Grid>
+        </Grow>
+      </Grid>
     );
-}
+  };
+  return (
+    <div className="bgcom container flex-center" id="technical-skills">
+      <Grid
+        container
+        justifyContent="center"
+        alignItems="center"
+        className="paddingtopbottom"
+      >
+        <Grid
+          item
+          lg={12}
+          md={12}
+          className="animate__animated animate__fadeInUp"
+        >
+          <Paper elevation={12} className="technical-skills-paper">
+            <Grid
+              container
+              spacing={matchesmd ? 3 : 4}
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Grid item lg={12} sm={12} className="header">
+                <div>Technical Skills</div>
+              </Grid>
+              {information.map((item: TechnicalskillsProps, key: number) =>
+                renderItem(item, key)
+              )}
+            </Grid>
+          </Paper>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
 
-export default Technicalskills
+export default Technicalskills;
