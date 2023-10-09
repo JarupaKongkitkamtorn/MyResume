fec79b42c879e422135cfd8447d5505b98504965
่Jarupa Kongkitkamtorn <jarupa.kongkitkamtorn@gmail.com>
2023-10-09 09:05:14 +0700

    Update

--- src/const/Information.tsx
+++ src/const/Information.tsx
@@ -1,223 +1,231 @@
-
-import javascript from '../image/program/javascript.png';
-import react from '../image/program/react.png';
-import nextjs from '../image/program/next-js.png';
-import redux from '../image/program/redux.svg';
-import HTML from '../image/program/HTML.png';
-import Bootstrap from '../image/program/Bootstrap.png';
-import materialui from '../image/program/material-ui.png';
-import semanticui from '../image/program/semanticui.png';
-import primereact from '../image/program/primereact.webp';
-import auth0 from '../image/program/auth0.png';
-import firebase from '../image/program/firebase.svg';
-import figma from '../image/program/figma.png';
-import cypress from '../image/program/cypress.png';
-import Git from '../image/program/Git.png';
-import aws from '../image/program/aws.png';
-import VSCode from '../image/program/VSCode.png';
-import GraphQL from '../image/program/GraphQL.png';
-import Bitbucket from '../image/program/Bitbucket.png';
-import jira from '../image/program/jira.png';
-import strapi from '../image/program/strapi.svg';
-import i18next from '../image/program/i18next.png';
-import apollo from '../image/program/apollo.png';
-import github from '../image/program/github.jpg';
-import gitlab from '../image/program/gitlab.png';
-import biosci from '../image/Biosci-logo.png';
-import vsni from '../image/project/Vsni logow.png';
-import farmtrust from '../image/project/Farmtrust_logo-03.png';
-import mma from '../image/project/MMA.png';
+import javascript from "../image/program/javascript.png";
+import react from "../image/program/react.png";
+import nextjs from "../image/program/next-js.png";
+import redux from "../image/program/redux.svg";
+import HTML from "../image/program/HTML.png";
+import Bootstrap from "../image/program/Bootstrap.png";
+import materialui from "../image/program/material-ui.png";
+import semanticui from "../image/program/semanticui.png";
+import primereact from "../image/program/primereact.webp";
+import auth0 from "../image/program/auth0.png";
+import firebase from "../image/program/firebase.svg";
+import figma from "../image/program/figma.png";
+import cypress from "../image/program/cypress.png";
+import Git from "../image/program/Git.png";
+import aws from "../image/program/aws.png";
+import VSCode from "../image/program/VSCode.png";
+import GraphQL from "../image/program/GraphQL.png";
+import Bitbucket from "../image/program/Bitbucket.png";
+import jira from "../image/program/jira.png";
+import strapi from "../image/program/strapi.svg";
+import i18next from "../image/program/i18next.png";
+import apollo from "../image/program/apollo.png";
+import github from "../image/program/github.jpg";
+import gitlab from "../image/program/gitlab.png";
+import biosci from "../image/Biosci-logo.png";
+import vsni from "../image/project/Vsni logow.png";
+import farmtrust from "../image/project/Farmtrust_logo-03.png";
+import mma from "../image/project/MMA.png";
 
 export const information = {
-    welcome: "Welcome to my identity.",
-    name: "Jarupa Kongkitkamtorn",
-    role: "I'm a Front-end Developer",
-    personality: "I create user-friendly websites using HTML, CSS, JavaScript, React, and Next.js. I prioritize web accessibility and collaborate with designers and back-end developers. My attention to detail and problem-solving skills make me a valuable addition to any web development team.",
-    phone: "(+66) 84-159-1444",
-    email: "jarupa.kongkitkamtorn@gmail.com",
-    linkedIn: "www.linkedin.com/in/jarupa-k",
-    github: "https://github.com/JarupaKongkitkamtorn",
-    education: {
-        bachelor: "Bachelor of Science (Computer Science)",
-        university: "King Mongkut’s University of Technology North Bangkok",
-        gpa: "GPA 3.13 (2014-2018)"
+  welcome: "Welcome to my identity.",
+  name: "Jarupa Kongkitkamtorn",
+  role: "I'm a Front-end Developer",
+  personality:
+    "I create user-friendly websites using HTML, CSS, JavaScript, React, and Next.js. I prioritize web accessibility and collaborate with designers and back-end developers. My attention to detail and problem-solving skills make me a valuable addition to any web development team.",
+  phone: "(+66) 84-159-1444",
+  email: "jarupa.kongkitkamtorn@gmail.com",
+  linkedIn: "www.linkedin.com/in/jarupa-k",
+  github: "https://github.com/JarupaKongkitkamtorn",
+  education: {
+    bachelor: "Bachelor of Science (Computer Science)",
+    university: "King Mongkut’s University of Technology North Bangkok",
+    gpa: "GPA 3.13 (2014-2018)",
+  },
+  certification: [
+    "Google Adwords 2017",
+    "Join Co-operative Education 2017",
+    "Join Gosoft Present Project Co-operative 2017",
+    "End the Co-operative Education form Dynamic Logistics Co., Ltd. 2017",
+    "QCC Project Simplify System Design first runner-up from Dynamic Logistics Co., Ltd. 2017",
+    "Start Up Thailand 2018 to the semi-final round, Project Chain_AID",
+  ],
+  training: [
+    "Google Adwords 2017",
+    "Start Up Thailand 2018",
+    "AWS Public Sector Summit ASEAN – Singapore 2019",
+  ],
+  technicalskills: [
+    {
+      icon: javascript,
+      label: "Java Script",
     },
-    certification: [
-        "Google Adwords 2017",
-        "Join Co-operative Education 2017",
-        "Join Gosoft Present Project Co-operative 2017",
-        "End the Co-operative Education form Dynamic Logistics Co., Ltd. 2017",
-        "QCC Project Simplify System Design first runner-up from Dynamic Logistics Co., Ltd. 2017",
-        "Start Up Thailand 2018 to the semi-final round, Project Chain_AID"
-    ],
-    training: [
-        "Google Adwords 2017",
-        "Start Up Thailand 2018",
-        "AWS Public Sector Summit ASEAN – Singapore 2019"
-    ],
-    technicalskills: [
+    {
+      icon: react,
+      label: "React",
+    },
+    {
+      icon: nextjs,
+      label: "Next.js",
+    },
+    {
+      icon: redux,
+      label: "Redux",
+    },
+    {
+      icon: HTML,
+      label: "HTML",
+    },
+    {
+      icon: Bootstrap,
+      label: "Bootstrap",
+    },
+    {
+      icon: materialui,
+      label: "Material UI",
+    },
+    {
+      icon: semanticui,
+      label: "Semantic UI",
+    },
+    {
+      icon: primereact,
+      label: "Primer React",
+    },
+    {
+      icon: auth0,
+      label: "Auth0",
+    },
+    {
+      icon: firebase,
+      label: "Firebase",
+    },
+    {
+      icon: figma,
+      label: "Figma",
+    },
+    {
+      icon: cypress,
+      label: "Cypress",
+    },
+    {
+      icon: Git,
+      label: "Git",
+    },
+    {
+      icon: aws,
+      label: "AWS",
+    },
+    {
+      icon: VSCode,
+      label: "Visual Studio Code",
+    },
+    {
+      icon: GraphQL,
+      label: "GraphQL",
+    },
+    {
+      icon: Bitbucket,
+      label: "Bitbucket",
+    },
+    {
+      icon: jira,
+      label: "Jira Software",
+    },
+    {
+      icon: strapi,
+      label: "Strapi",
+    },
+    {
+      icon: i18next,
+      label: "react-i18next",
+    },
+    {
+      icon: apollo,
+      label: "@apollo/client",
+    },
+    {
+      icon: github,
+      label: "Github",
+    },
+    {
+      icon: gitlab,
+      label: "gitlab",
+    },
+  ],
+  experience: [
+    {
+      icon: biosci,
+      dateTime: "Jun 2018 - Present",
+      company: "Biosci(Thailand) Co., Ltd.",
+      role: "Intermediate Front-end Developer",
+      description:
+        "My main role is to create attractive and user-friendly websites. I'm skilled in HTML, CSS, and JavaScript, which helps me turn design ideas into functional web pages. I also use modern tools like React and Next.js to build interactive interfaces. I prioritize web accessibility to ensure everyone can use the sites I create. Collaboration with designers and back-end developers is crucial to ensure a seamless website experience. With my attention to detail and problem-solving abilities, I'm a valuable addition to any development team, dedicated to crafting engaging web designs.",
+      project: [
         {
-            icon: javascript,
-            label: "Java Script"
+          icon: vsni,
+          link: "https://vsni.co.uk/",
+          title: "VSN International Website",
+          description: "A company website",
+          tech: "Next.js, AWS (S3, SES - send an email, Cloudfront), Strapi (CMS)",
         },
         {
-            icon: react,
-            label: "React"
+          icon: farmtrust,
+          link: "https://www.farm-trust.com/",
+          title: "Farm Trust Dashboard",
+          description:
+            "It's a part of the Farm Trust application for administrators to manage farmers and the data collected from the app. This includes further analysis of the data, such as presenting it in easy-to-understand graphs and utilizing Google Maps for visualization.",
+          tech: "React, Firebase, Apollo Client",
         },
         {
-            icon: nextjs,
-            label: "Next.js"
+          icon: mma,
+          link: "https://www.mixedmodel.academy/",
+          title: "Mix Model Academy",
+          description:
+            "An easy-to-understand code generation system for statisticians to produce code that can be integrated into the company's software to generate statisticalresults.",
+          tech: "React, AWS (Amplify, Appsync, S3, Cloudfront, Cognito, DynamoDB)",
         },
         {
-            icon: redux,
-            label: "Redux"
+          icon: "",
+          link: "",
+          title: "Mixed Model Academy Dashboard",
+          description:
+            "It's a dashboard displaying raw data for the marketing team to further analyze.",
+          tech: "React, AWS (Amplify, Appsync, S3, Cloudfront, Cognito, DynamoDB)",
         },
         {
-            icon: HTML,
-            label: "HTML"
+          icon: "",
+          link: "",
+          title: "Animal Breeding Platform",
+          description:
+            "A system that aids in analyzing data and managing animal breeding to enhance the quality of animal breeds in terms of genetics.",
+          tech: "React, Django",
         },
         {
-            icon: Bootstrap,
-            label: "Bootstrap"
+          icon: "",
+          link: "",
+          title: "Site Reference Generator",
+          description:
+            "Generate a reference code system for activating the company's software.",
+          tech: "React, Auth0",
         },
         {
-            icon: materialui,
-            label: "Material UI"
+          icon: "",
+          link: "",
+          title: "VSNdigital Monitor Dashboard",
+          description:
+            "A license management system for displaying various in ßformation such as expiration and renewal.",
+          tech: "React",
         },
         {
-            icon: semanticui,
-            label: "Semantic UI"
+          icon: "",
+          link: "",
+          title: "Account Dashboard",
+          description:
+            "A customer dashboard for self-managing licenses, similar to a userprofile page.",
+          tech: "React, Auth0",
         },
-        {
-            icon: primereact,
-            label: "Primer React"
-        },
-        {
-            icon: auth0,
-            label: "Auth0"
-        },
-        {
-            icon: firebase,
-            label: "Firebase"
-        },
-        {
-            icon: figma,
-            label: "Figma"
-        },
-        {
-            icon: cypress,
-            label: "Cypress"
-        },
-        {
-            icon: Git,
-            label: "Git"
-        },
-        {
-            icon: aws,
-            label: "AWS"
-        },
-        {
-            icon: VSCode,
-            label: "Visual Studio Code"
-        },
-        {
-            icon: GraphQL,
-            label: "GraphQL"
-        },
-        {
-            icon: Bitbucket,
-            label: "Bitbucket"
-        },
-        {
-            icon: jira,
-            label: "Jira Software"
-        },
-        {
-            icon: strapi,
-            label: "Strapi"
-        },
-        {
-            icon: i18next,
-            label: "react-i18next"
-        },
-        {
-            icon: apollo,
-            label: "@apollo/client"
-        },
-        {
-            icon: github,
-            label: "Github"
-        },
-        {
-            icon: gitlab,
-            label: "gitlab"
-        },
-    ],
-    experience: [
-        {
-            icon: biosci,
-            dateTime: "Jun 2018 - Present",
-            company: "Biosci(Thailand) Co., Ltd.",
-            role: "Intermediate Front-end Developer",
-            description: "My main role is to create attractive and user-friendly websites. I'm skilled in HTML, CSS, and JavaScript, which helps me turn design ideas into functional web pages. I also use modern tools like React and Next.js to build interactive interfaces. I prioritize web accessibility to ensure everyone can use the sites I create. Collaboration with designers and back-end developers is crucial to ensure a seamless website experience. With my attention to detail and problem-solving abilities, I'm a valuable addition to any development team, dedicated to crafting engaging web designs.",
-            project: [
-                {
-                    icon: vsni,
-                    link: "https://vsni.co.uk/",
-                    title: "VSN International Website",
-                    description: "A company website",
-                    tech: "Next.js, AWS (S3, SES - send an email, Cloudfront), Strapi (CMS)"
-                },
-                {
-                    icon: farmtrust,
-                    link: "https://www.farm-trust.com/",
-                    title: "Farm Trust Dashboard",
-                    description: "It's a part of the Farm Trust application for administrators tomanage farmers and the data collected from the app. This includes further analysis of thedata, such as presenting it in easy-to-understand graphs and utilizing Google Maps forvisualization.",
-                    tech: "React, Firebase, Apollo Client"
-                },
-                {
-                    icon: mma,
-                    link: "https://www.mixedmodel.academy/",
-                    title: "Mix Model Academy",
-                    description: "An easy-to-understand code generation system for statisticians toproduce code that can be integrated into the company's software to generate statisticalresults.",
-                    tech: "React, AWS (Amplify, Appsync, S3, Cloudfront, Cognito, DynamoDB)"
-                },
-                {
-                    icon: "",
-                    link: "",
-                    title: "Mixed Model Academy Dashboard",
-                    description: "It's a dashboard displaying raw data for the marketingteam to further analyze.",
-                    tech: "React, AWS (Amplify, Appsync, S3, Cloudfront, Cognito, DynamoDB)"
-                },
-                {
-                    icon: "",
-                    link: "",
-                    title: "Animal Breeding Platform",
-                    description: "A system that aids in analyzing data and managing animalbreeding to enhance the quality of animal breeds in terms of genetics.",
-                    tech: "React, Django"
-                },
-                {
-                    icon: "",
-                    link: "",
-                    title: "Site Reference Generator",
-                    description: "Generate a reference code system for activating the company'ssoftware.",
-                    tech: "React, Auth0"
-                },
-                {
-                    icon: "",
-                    link: "",
-                    title: "VSNdigital Monitor Dashboard",
-                    description: "A license management system for displaying variousinformation such as expiration and renewal.",
-                    tech: "React"
-                },
-                {
-                    icon: "",
-                    link: "",
-                    title: "Account Dashboard",
-                    description: "A customer dashboard for self-managing licenses, similar to a userprofile page.",
-                    tech: "React, Auth0"
-                },
-            ]
-        }
-    ]
-}
+      ],
+    },
+  ],
+};
